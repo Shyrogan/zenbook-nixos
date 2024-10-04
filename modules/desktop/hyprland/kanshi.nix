7e6e17f0766f93a878c8051dabfa98274ca4ffdb
Sébastien VIAL <sebastien@shyrogan.fr>
2024-10-04 18:01:44 +0200

    feat: kanshi position

--- modules/desktop/hyprland/kanshi.nix
+++ modules/desktop/hyprland/kanshi.nix
@@ -6,8 +6,8 @@
         profile.name = "docked";
         profile.outputs = [
           { criteria = "eDP-1"; status = "disable"; }
-          { criteria = "Iiyama North America PL2488H 1134243101441"; transform = "90"; status = "enable"; position = "1920x-540"; }
-          { criteria = "LG Electronics LG ULTRAGEAR 101NTKFEN429"; scale = 1.33333333; status = "enable"; }
+          { criteria = "Iiyama North America PL2488H 1134243101441"; position = "1920,-540"; transform = "90"; status = "enable"; }
+          { criteria = "LG Electronics LG ULTRAGEAR 101NTKFEN429"; position = "0,0"; scale = 1.33333333; status = "enable"; }
         ];
       }
       {
