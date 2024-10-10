2d615449ba6399bcb95a7158aea2af96d88b4bf4
Sébastien VIAL <sebastien@shyrogan.fr>
2024-10-10 20:44:16 +0200

    chore: fix display

--- modules/desktop/hyprland/kanshi.nix
+++ modules/desktop/hyprland/kanshi.nix
@@ -5,8 +5,8 @@
       {
         profile.name = "3screens";
         profile.outputs = [
-          { criteria = "eDP-1"; position = "-1920,0"; scale = 2.00; status = "enable"; }
-          { criteria = "Iiyama North America PL2488H 1134243101441"; position = "1920,-540"; transform = "90"; status = "enable"; }
+          { criteria = "eDP-1"; position = "-1440,0"; scale = 2.00; status = "enable"; }
+          { criteria = "Iiyama North America PL2488H 0"; position = "1920,-540"; transform = "90"; status = "enable"; }
           { criteria = "LG Electronics LG ULTRAGEAR 101NTKFEN429"; position = "0,0"; scale = 1.33333333; status = "enable"; }
         ];
       }
@@ -14,7 +14,7 @@
         profile.name = "2screens";
         profile.outputs = [
           { criteria = "eDP-1"; position = "-1920,0"; scale = 2.00; status = "disable"; }
-          { criteria = "Iiyama North America PL2488H 1134243101441"; position = "1920,-540"; transform = "90"; status = "enable"; }
+          { criteria = "Iiyama North America PL2488H 0"; position = "1920,-540"; transform = "90"; status = "enable"; }
           { criteria = "LG Electronics LG ULTRAGEAR 101NTKFEN429"; position = "0,0"; scale = 1.33333333; status = "enable"; }
         ];
       }
