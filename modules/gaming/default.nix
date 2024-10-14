5f9300bb84083766a7617cd16f6b4049128f0d3f
Sébastien VIAL <sebastien@shyrogan.fr>
2024-10-14 15:21:33 +0200

    chore: enable appimage for gaming

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -5,5 +5,8 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
-  programs.appimage.binfmt = true;
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
 }
