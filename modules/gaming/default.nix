694d571f1962cafa85914a9e01e0ecf1c1c630b4
Sébastien VIAL <sebastien@shyrogan.fr>
2024-10-15 21:44:43 +0200

    chore: updates

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -9,16 +9,4 @@
     enable = true;
     binfmt = true;
   };
-
-  environment.systemPackages = with pkgs; [stdenv.mkDerivation {
-    name = "ankama-launcher";
-    src = fetchurl {
-      src = "https://launcher.cdn.ankama.com/installers/production/Ankama%20Launcher-Setup-x86_64.AppImage";
-      sha256 = "00mag1kjvqcxhs50fwvjyw5wvrkn8lagbr1lzkqg8rgac1qzb6p3";
-    };
-    installPhase = ''
-      mkdir -p $out/bin
-      cp Ankama\ Launcher-Setup-x86_64.AppImage $out/bin/ankama-launcher.AppImage
-    '';
-  }];
 }
