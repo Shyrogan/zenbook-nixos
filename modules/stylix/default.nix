bb11201dcd4a312e8b156af3d4745f3fdfbd77bb
Sébastien VIAL <sebastien@shyrogan.fr>
2024-10-30 16:45:09 +0100

    chore: going back to old wallpaper

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -4,7 +4,7 @@
   ];
   stylix = {
     enable = true;
-    image = ../../assets/wallpaper_dark.jpg;
+    image = ../../assets/wallpaper_dark.png;
     base16Scheme = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream.yaml";
       sha256 = "1bfi479g7v5cz41d2s0lbjlqmfzaah68cj1065zzsqksx3n63znf";
